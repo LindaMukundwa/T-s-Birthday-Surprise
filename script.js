ef0c3b9e2b547f35c5fdbd4518248e13e3883a5b
LindaMukundwa <129442651+LindaMukundwa@users.noreply.github.com>
2024-09-01 21:35:44 -0400

    better format

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('#button');
 const canvas = document.querySelector('#confetti');
+const happy = document.querySelector('.happy');
 
 const jsConfetti = new JSConfetti();
 
@@ -7,4 +8,8 @@ button.addEventListener('click', () => {
     jsConfetti.addConfetti({
         emojis: ['💰', '🏃🏾', '💥', '🇳🇬', '🖤', '🌸', '🥳','🍾','❤️‍🔥','🙂↔️'],
     }).then(() => jsConfetti.addConfetti())
+})
+
+happy.addEventListener('click', () => {
+    window.location.href="https://www.casamigos.com/en-us";
 })
