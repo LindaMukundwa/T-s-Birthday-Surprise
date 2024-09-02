834a8fa69d2aff2f13c7c049007e960471811144
LindaMukundwa <129442651+LindaMukundwa@users.noreply.github.com>
2024-09-01 20:18:11 -0400

    very funny idea

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti({
+        emojis: ['💰', '🏃🏾', '💥', '🇳🇬', '🖤', '🌸'],
+    }).then(() => jsConfetti.addConfetti())
+})
