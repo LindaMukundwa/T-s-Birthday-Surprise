31608994d3b1c9372cc4138459c4702816bf35c3
LindaMukundwa <129442651+LindaMukundwa@users.noreply.github.com>
2024-09-01 21:25:00 -0400

    more changes

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ const jsConfetti = new JSConfetti();
 
 button.addEventListener('click', () => {
     jsConfetti.addConfetti({
-        emojis: ['💰', '🏃🏾', '💥', '🇳🇬', '🖤', '🌸'],
+        emojis: ['💰', '🏃🏾', '💥', '🇳🇬', '🖤', '🌸', '🥳','🍾','❤️‍🔥','🙂↔️'],
     }).then(() => jsConfetti.addConfetti())
 })
